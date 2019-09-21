e4231bff5808fd0a74f8a56b2b1730875e424e77
Mengqi Wang <wmq516@gmail.com>
2019-09-21 00:52:33 -0600

    changed config.js

--- src/config.js
+++ src/config.js
@@ -4,8 +4,6 @@ module.exports = {
   DB_URL: process.env.DATABASE_URL || 'postgresql://Mengqi89@localhost/everest',
   JWT_SECRET: process.env.JWT_SECRET || 'my-own-special-jwt-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '20s',
-  API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
-    "http://localhost:8000/api",
   CLIENT_ORIGIN: 'https://everest.wmq516.now.sh',
   authToken_Teacher: process.env.authToken_Teacher,
   authToken_School: process.env.authToken_School,
